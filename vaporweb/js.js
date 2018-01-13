b45aac0701eea41964e2a86f1a70ffb90a2905ce
Stephanie Hu <stephaniehu@stephanies-mbp.risd.edu>
2018-01-13 15:10:48 -0500

    p2 changes

--- vaporweb/js.js
+++ vaporweb/js.js
@@ -1,6 +1,22 @@
+var driving = false;
+
 function hover(element) {
     element.setAttribute('src', 'active.png');
 }
 function unhover(element) {
     element.setAttribute('src', 'inactive.png');
 }
+function stopDrive() {
+	var el = document.getElementById("inactive");
+	el.setAttribute('src', 'active.png');
+	var body = document.getElementById('dashbox');
+	body.style.backgroundImage = 'url("inactivebg.png")';
+}
+function drive() {
+	var el = document.getElementById("inactive");
+	el.setAttribute('src', 'driving.png');
+	var body = document.getElementById('dashbox');
+	body.style.backgroundImage = 'url("activebg.png")';
+	setTimeout(stopDrive, 10000);
+}
+window.addEventListener('scroll',drive, true);
