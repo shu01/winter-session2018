b7e314100a4e90c03c8428cf14b7a02fec5ac54d
Stephanie Hu <stephaniehu@Stephanies-MacBook-Pro.local>
2018-01-15 12:37:08 -0500

    img attempts

--- vaporweb/js.js
+++ vaporweb/js.js
@@ -1,10 +1,19 @@
 var driving = false;
 var element = document.querySelector('#dashbox');
 var audio = new Audio('music.mp3');
+var activeImages = [];
 
 function genNum() {
     var x = Math.floor((Math.random() * 10) + 1);
     return x;
+}
+function genX() {
+	var x = Math.floor((Math.random() * 700) + 1);
+	return x;
+}
+function genY() {
+	var y = Math.floor((Math.random() * 1000) + 1);
+	return y;
 }
 function randLoc(){
 	var x = genNum();
@@ -30,14 +39,23 @@ function drive() {
 	var body = document.getElementById('dashbox');
 	body.style.backgroundImage = 'url("activebg.gif")';
 	audio.play();
-	///setTimeout(stopDrive, 10000);
 }
 function newImg(){
-	var rl = randLoc()
+	var rl = randLoc();
+	var elem = document.getElementsByClassName("imag");
+	elem.backgroundImage = 'url("img/2.jpg")';
+}
+/*function newImg(){
+	var rl = randLoc();
 	var elem = document.createElement("img");
-	elem.setAttribute("src", rl);
-	elem.setAttribute("id", "image");
-	document.getElementById("dashbox").appendChild(elem);
+	elem.setAttribute("src", "img/1.jpg");
+	elem.setAttribute("class", "image");
+	setTimeout(function() {
+    	document.getElementById("dashbox").appendChild(elem);
+    	}, 100);
+}
+
+/*
 }
 function fadeInInc(opacity){
 	var elem = document.getElementById("image");
@@ -47,23 +65,20 @@ function fadeInInc(opacity){
 function fadeIn(){
 	var elem = document.getElementById("image");
 	var opacity = 0; 
-	while (opacity <= 1){
-	var newOp = setTimeout(fadeInInc(opacity), 3000);}
+	if (opacity <= 1){
+		var newOp =fadeInInc(opacity)}
 	opacity = newOp
 	elem.style.opacity = newOp;
 }
-
-///window.addEventListener('scroll',drive, true);
+*/
 (function( $ ) {
 	$(function() {
-		var $output = $( "#output" ),
-			scrolling = "<span id='scrolling'>Scrolling</span>",
-			stopped = "<span id='stopped'>Stopped</span>";
+		var $output = $( "#output" );
 
 		    $( window ).scroll(function() {
 		    	drive();
 		    	newImg();
-		    	fadeIn();
+		    	window.setInterval(function(){newImg()}, 5000);
     			clearTimeout( $.data( this, "scrollCheck" ) );
     			$.data( this, "scrollCheck", setTimeout(function() {
     				stopDrive();
