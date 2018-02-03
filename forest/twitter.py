321e2b2a78567c9849d1322a872a19fe6cdb2aea
Stephanie Hu <stephaniehu@Stephanies-MacBook-Pro.local>
2018-02-02 21:28:34 -0500

    updates

--- forest/twitter.py
+++ forest/twitter.py
@@ -3,6 +3,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
+tweets = []
 
 #Variables that contains the user credentials to access Twitter API 
 access_token = "958434177736740871-Zu1DYe88RpXruMDpJtHA2h1bCXodCdo"
@@ -12,15 +13,19 @@ consumer_secret = "ZnlXSLHPZouvovxtUQkXlnz4kUlFTJ0bmxbYWJqnAYPtpzlQ8o"
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
+    save_file = open('text.json', 'a')
 
     def on_data(self, data):
         json_load = json.loads(data)
         texts = json_load['text']
         coded = texts.encode('utf-8')
         s = str(coded)
-        print(s[2:-1])
+        #print(s[2:-1])
+        data=[]
+        data.append((s[2:-1]))
+        with open('data.json', 'w') as json_data: # 'w' to open for writing
+            json.dump(data, json_data)
         return True
-
     def on_error(self, status):
         print (status)
 
