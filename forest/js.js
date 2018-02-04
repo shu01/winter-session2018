04bb11f26a41f8388e794eaf334f337765843035
Stephanie Hu <stephaniehu@Stephanies-MacBook-Pro.local>
2018-02-04 02:16:01 -0500

    add audio

---
+++ forest/js.js
@@ -0,0 +1,41 @@
+var ss = [];
+
+bg = new Audio('noise/ambience.mp3');
+bg.loop = true;
+bg.play();
+
+a0 = new Audio('noise/0.mp3');
+a1 = new Audio('noise/1.mp3');
+a2 = new Audio('noise/2.mp3');
+a3 = new Audio('noise/3.mp3');
+a4 = new Audio('noise/4.mp3');
+a5 = new Audio('noise/5.mp3');
+a6 = new Audio('noise/6.mp3');
+a7 = new Audio('noise/7.mp3');
+
+var sounds = [a0,a1,a2,a3,a4,a5,a6,a7];
+
+function checkFile(index){
+	for (var s in sounds){
+		if (s == index){
+			s.play;
+			console,log(s);
+		}
+	}
+}
+
+function loadlink(){
+    $('#data').load('data.txt',function () {
+         $(this).unwrap();
+         var str = $( "p:first" ).text();
+         ss = str.split(",");
+         last = (ss[ss.length-1]);
+         checkFile(last);
+
+    });
+}
+
+loadlink(); // This will run on page load
+setInterval(function(){
+    loadlink() // this will run after every 5 seconds
+}, 50);
--- forest/twitter.py
+++ forest/twitter.py
@@ -3,7 +3,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
-tweets = []
+data_array=[]
 
 #Variables that contains the user credentials to access Twitter API 
 access_token = "958434177736740871-Zu1DYe88RpXruMDpJtHA2h1bCXodCdo"
@@ -11,24 +11,62 @@ access_token_secret = "zaCsZPH1y7Q0svdFAjbgOr0VruSUUQaHhA5cSRhRLCYoB"
 consumer_key = "8zpPkup54geZPCOJvcAEMxA5D"
 consumer_secret = "ZnlXSLHPZouvovxtUQkXlnz4kUlFTJ0bmxbYWJqnAYPtpzlQ8o"
 
+page_template = """
+<html>
+<body id="data">
+%s
+</body>
+</html>"""
+ 
+def StringParse(string):
+    s = str(string)
+    length = len(s)
+    return(length)
 #This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
-    save_file = open('text.json', 'a')
 
+def Sort(length):
+    if length <= 70:
+        return("a0")
+    elif length <= 90:
+        return("a1")
+    elif length <= 110:
+        return("a2")
+    elif length <= 130:
+        return("a3")
+    elif length <= 150:
+        return("a4")
+    elif length <= 170:
+        return("a5")
+    elif length <= 190:
+        return("a6")
+    else:
+        return("a7")
+
+
+class StdOutListener(StreamListener):
+    #save_file = open('text.json', 'a')
     def on_data(self, data):
         json_load = json.loads(data)
         texts = json_load['text']
         coded = texts.encode('utf-8')
-        s = str(coded)
+        coded_str = str(coded)
+        s = (coded_str[2:-1])
+        num = StringParse(s)
+        fileNum = Sort(num)
         #print(s[2:-1])
-        data=[]
-        data.append((s[2:-1]))
-        with open('data.json', 'w') as json_data: # 'w' to open for writing
-            json.dump(data, json_data)
+        data_array.append(fileNum)
+        #with open('data.json', 'a') as json_data: # 'w' to open for writing
+         #   a = {'data': data}
+          #  json.dump(a, json_data)
+        #str_data=str(data_array)
+        #new_html = page_template%(str_data)
+        Html_file= open("data.txt","w")
+        #Html_file.write(new_html)
+        Html_file.write(str(data_array))
+        Html_file.close()
         return True
     def on_error(self, status):
         print (status)
-
 
 if __name__ == '__main__':
 
